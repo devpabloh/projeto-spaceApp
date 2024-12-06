fa7e826ee5d28a1f53ca826a6c51ac35270295b6
pablo <devpabloh@gmail.com>
2024-12-05 23:14:14 -0300

    criando os estilos do fundo do projeto

--- space-app/src/App.jsx
+++ space-app/src/App.jsx
@@ -2,6 +2,7 @@ import {styled} from "styled-components"
 
 const FundoGradient = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+  width: 100%;
 `
 
 function App() {
