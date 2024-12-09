9e09dae522ec3bfea590cad3a2e3b141ffb402e9
Pablo Henrique <pablohenriqueadm94@gmail.com>
2024-12-09 18:06:37 -0300

    criando estrutura do componente imagem

---
+++ space-app/src/componentes/Galeria/Imagem/index.jsx
@@ -0,0 +1,16 @@
+const Imagem = ({foto})=>{
+    return(
+        <figure>
+            <figcaption>
+                <h3>Titulo</h3>
+                <footer>
+                    <p>fonte</p>
+                    <button>Favorito</button>
+                    <button>Expandir</button>
+                </footer>
+            </figcaption>
+        </figure>
+    )
+}
+
+export default Imagem
